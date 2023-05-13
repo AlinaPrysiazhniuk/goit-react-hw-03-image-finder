ee2ce7799d03593d963042118422ecb48a456ab9
Alina Prysiazhnuyk <alnkaprisiazhnuyk@gmail.com>
2023-05-13 18:14:54 +0300

    search

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    image: null,
+  };
+
+  componentDidMount() {
+    fetch(
+      'https://pixabay.com/api/?q=cat&page=1&key=34983998-155dfb76bac09cdf48f99cd2f&image_type=photo&orientation=horizontal&per_page=12'
+    )
+      .then(res => res.json())
+      .then(image => this.setState({ image }));
+  }
+
+  render() {
+    return <div>sasas</div>;
+  }
+}
